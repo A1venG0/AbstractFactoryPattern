5da942d8fdce46750c1fa8823fa60ad4920cc14d
A1venG0 <artemlev11@ukr.net>
2022-10-19 17:40:40 +0300

    Added: abstact factory test

--- Main.java
+++ Main.java
@@ -3,7 +3,21 @@ public class Main
 
     public static void main(String[] args)
     {
-        WhiteCircle cir = new WhiteCircle();
-        System.out.println(cir.toString());
+        BaseFactory factory;
+
+        // choose between white and black factories
+
+        factory = new WhiteFactory();
+
+        //factory = new BlackFactory();
+
+        Triangle tr;
+        Circle c;
+        tr = factory.createTriangle();
+        c = factory.createCircle();
+
+        System.out.println(tr.toString());
+
+        System.out.println(c.toString());
     }
 }
