4bddd47aa3b8150fce3aa89e3bf3fd7c3fc40804
A1venG0 <artemlev11@ukr.net>
2022-10-25 11:27:34 +0300

    Changed: radius is now protected

--- BlackCircle.java
+++ BlackCircle.java
@@ -10,7 +10,7 @@ public class BlackCircle extends Circle
 	public String toString()
 	{
 		return "Black circle with radius: "
-		+ radius
+		+ getRadius()
 		+ '\n';
 	}
 }
--- Circle.java
+++ Circle.java
@@ -1,7 +1,21 @@
 public abstract class Circle
 {
-	
 	protected int radius;
+
+	public int getRadius() { return this.radius; }
+
+	//public void setRadius(int radius) { this.radius = radius; }
+
+	public Circle(int radius) 
+	{
+		this.radius = radius;
+		//setRadius(radius);
+	}
+
+	public Circle()
+	{
+		this.radius = radius;
+	}
 
 	public double getArea()
 	{
