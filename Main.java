822683bcc54cf91cacaad3872d81c3ad1fcfb7f6
A1venG0 <artemlev11@ukr.net>
2022-10-25 12:17:01 +0300

    Added: use of random and AbstractFactory

--- Main.java
+++ Main.java
@@ -1,23 +1,31 @@
+import java.util.Random;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-        BaseFactory factory;
+        Random rand = new Random();
 
-        // choose between white and black factories
+        AbstractFactory absFactory = new AbstractFactory();
+        BaseFactory factory = absFactory.getFactory(rand.nextInt(2));
 
-        factory = new WhiteFactory();
+        Triangle tr = factory.createTriangle(12, 16, 20); // creating a tringle using abstract factory
+        Circle c = factory.createCircle(5); // creating a circle using abstract factory
 
-        //factory = new BlackFactory();
+        System.out.print(tr.toString());
+        System.out.print("Triangle perimeter is: ");
+        System.out.println(tr.getPerimeter());
+        System.out.print("Triangle area is: ");
+        System.out.println(tr.getArea());
+        System.out.println("-----------------------------");
 
-        Triangle tr;
-        Circle c;
-        tr = factory.createTriangle();
-        c = factory.createCircle();
+        System.out.print(c.toString());
+        System.out.print("Circle perimeter is: ");
+        System.out.println(c.getPerimeter());
+        System.out.print("Circle area is: ");
+        System.out.println(c.getArea());
 
-        System.out.println(tr.toString());
 
-        System.out.println(c.toString());
     }
 }
