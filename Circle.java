02ebbb81eb6507b0bddceaf789893bd8db29cfd7
A1venG0 <artemlev11@ukr.net>
2022-10-26 21:50:13 +0300

    Added: Shape implementation

--- Circle.java
+++ Circle.java
@@ -1,14 +1,16 @@
-public abstract class Circle
+public abstract class Circle implements Shape
 {
 	protected int radius;
-
+	
 	public int getRadius() { return this.radius; }
 
+	@Override
 	public double getArea()
 	{
 		return Math.PI * radius * radius;
 	}
 
+	@Override
 	public double getPerimeter()
 	{
 		return 2 * Math.PI * radius;
